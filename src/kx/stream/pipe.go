0a2aa1f316fc163ebf14c1e5f74463c5069ac3a5
funkygao <funky.gao@gmail.com>
2012-10-16 13:18:36 +0800

    stream pkg

---
+++ src/kx/stream/pipe.go
@@ -0,0 +1,66 @@
+// codable stream or pipe
+package stream
+
+import (
+    "bufio"
+    "os/exec"
+)
+
+// Stream data
+type Stream struct {
+    name string // command name
+    arg []string // command arguments
+    cmd *exec.Cmd
+    reader *bufio.Reader
+    writer *bufio.Writer
+}
+
+// constructor
+func NewStream(name string, arg ...string) *Stream {
+    this := &Stream{name: name, arg: arg}
+    return this
+}
+
+// open stream
+func (this *Stream) Open() {
+    this.cmd = exec.Command(this.name, this.arg...)
+
+    // stdout pipe
+    out, err := this.cmd.StdoutPipe()
+    if err != nil {
+        panic(err)
+    }
+
+    // stdin pipe
+    in, err := this.cmd.StdinPipe()
+    if err != nil {
+        panic(err)
+    }
+
+    // startup
+    if err := this.cmd.Start(); err != nil {
+        panic(err)
+    }
+
+    // prepare the reader/writer
+    this.reader = bufio.NewReader(out)
+    this.writer = bufio.NewWriter(in)
+}
+
+// get reader to read from the pipe output
+func (this Stream) GetReader() *bufio.Reader {
+    return this.reader
+}
+
+// get writer to write to the pipe input
+func (this Stream) GetWriter() *bufio.Writer {
+    return this.writer
+}
+
+// close the stream
+func (this *Stream) Close() {
+    if err := this.cmd.Wait(); err != nil {
+        panic(err)
+    }
+}
+
