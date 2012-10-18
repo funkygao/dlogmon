f6379cab94881ddcddefbd4f3f7bf108bcf526cf
funkygao <funky.gao@gmail.com>
2012-10-18 14:35:50 +0800

    tips

--- src/kx/sb/doc.go
+++ src/kx/sb/doc.go
@@ -1,4 +1,8 @@
 /*
 StringBuffer in go
+
+Attention:
+    Due to compiler-optimizations and depending on the size of the strings using a StringBuilder only 
+    starts to become more efficient when the number of iterations is > 15
 */
 package sb
