e710e830af682457ea938e01b9280a27e106922b
funky gao <funky.gao@gmail.com>
2012-10-18 22:34:55 +0800

    final benchmark

--- test/benchmark/chan.go
+++ test/benchmark/chan.go
@@ -3,7 +3,6 @@ package main
 
 import (
     "fmt"
-    "runtime"
     "strings"
     "time"
 )
@@ -14,7 +13,6 @@ const (
 
 type param struct {
     chbuf, datasize int
-    yield bool
 }
 
 func timeit(f func(param), p param) time.Duration {
@@ -34,10 +32,6 @@ func bench(p param) {
     go func() {
         for i:=0; i<LOOPS; i++ {
             c <- data
-
-            if p.yield {
-                runtime.Gosched()
-            }
         }
 
         close(c)
@@ -62,10 +56,8 @@ func runBenches() {
     for _, d := range []int{1, 128, 1024, 1024*1024} {
         fmt.Println("datasize:", d)
         for _, b := range []int{0, 1, 10, 1000, 10000} {
-            for _, y := range []bool{true, false} {
-                t := timeit(bench, param{b, d, y})
-                fmt.Printf("\t%10s  %5s: buf:%5d, yield:%v\n", t, t/LOOPS, b, y)
-            }
+            t := timeit(bench, param{b, d})
+            fmt.Printf("\t%10s  %5s: buf:%5d\n", t, t/LOOPS, b)
         }
     }
 }
