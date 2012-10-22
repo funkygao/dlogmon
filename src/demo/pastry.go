1a099376558886e6a50000bb873cf32fdaf0bb08
funkygao <funky.gao@gmail.com>
2012-10-22 13:19:18 +0800

    demo of some 3rd party pkg

---
+++ src/demo/pastry.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "secondbit.org/pastry"
+    "os"
+)
+
+func main() {
+    hostname, _ := os.Hostname()
+    println(hostname)
+
+    id, _ := pastry.NodeIDFromBytes([]byte(hostname))
+    node := pastry.NewNode(id, "121.121.12.12", "1.1.1.1", "region", 9000)
+    credentials := pastry.Passphrase("I <3 Gophers.")
+    cluster := pastry.NewCluster(node, credentials)
+    go func() {
+        defer cluster.Stop()
+        cluster.Listen()
+    }()
+
+    cluster.Join("2.2.23.3", 8080)
+    select {
+    }
+    
+}
