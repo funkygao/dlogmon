6a86e66f5f31ebd817e600fec5a40c1c2e34f8fb
funkygao <funky.gao@gmail.com>
2012-10-23 13:54:38 +0800

    parser extracted out

--- src/kx/dlog/amf_worker.go
+++ src/kx/dlog/amf_worker.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "kx/stats"
     t "kx/trace"
-    "strconv"
     "strings"
 )
 
@@ -30,37 +29,6 @@ func NewAmfWorker(manager *Manager, filename string) IWorker {
     this.combiner = stats.StatsSum
 
     return this
-}
-
-func (this *amfRequest) parseLine(line string) {
-    // major parts seperated by space
-    parts := strings.Split(line, " ")
-
-    // uri related
-    uriInfo := strings.Split(parts[5], "+")
-    if len(uriInfo) < 3 {
-        panic(uriInfo)
-    }
-    this.http_method, this.uri, this.rid = uriInfo[0], uriInfo[1], uriInfo[2]
-
-    // class call and args related
-    callRaw := strings.Replace(parts[6], "{", "", -1)
-    callRaw = strings.Replace(callRaw, "}", "", -1)
-    callRaw = strings.Replace(callRaw, "\"", "", -1)
-    callRaw = strings.Replace(callRaw, "[", "", -1)
-    callRaw = strings.Replace(callRaw, "]", "", -1)
-    callRaw = strings.Replace(callRaw, ",", ":", -1)
-    callInfo := strings.Split(callRaw, ":")
-    time, err := strconv.Atoi(callInfo[1])
-    if err != nil {
-        println(line)
-        panic(err)
-    }
-    this.time = int16(time)
-    this.class = callInfo[3]
-    if len(callInfo) > 10 {
-        this.method = callInfo[10]
-    }
 }
 
 // Does a log line contain 'AMF_SLOW'?
