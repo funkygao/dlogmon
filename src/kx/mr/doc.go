a1832d9e3d560b6f307ac3dc58a0705c9135ae5f
funky gao <funky.gao@gmail.com>
2012-10-24 22:30:39 +0800

    explaination of MapReduce

--- src/kx/mr/doc.go
+++ src/kx/mr/doc.go
@@ -1,6 +1,8 @@
 /*
 MapReduce related package.
 
+MapReduce = select key, aggr(value) from ... group by key order by key
+
 MapReduce in hadoop:
     map: (k1, v1) => [(k2, v2)] 
     reduce: (k2, [v2]) => [(k3, v3)]
