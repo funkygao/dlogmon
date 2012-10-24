477792959e08a12b43844303ca6eac3d4bf49b8b
funky gao <funky.gao@gmail.com>
2012-10-24 22:53:14 +0800

    map reduce explained in detail

--- src/kx/mr/doc.go
+++ src/kx/mr/doc.go
@@ -6,5 +6,39 @@ MapReduce = select key, aggr(value) from ... group by key order by key
 MapReduce in hadoop:
     map: (k1, v1) => [(k2, v2)] 
     reduce: (k2, [v2]) => [(k3, v3)]
+
+WordCount as example:
+                    lines
+       -------------------------------
+      |               |               |
+      V               V               V
+    mapper          mapper          mapper
+      |               |               |
+      V               V               V
+    a[1] b[2]    a[2] c[5]       b[2] a[6] c[9]
+    -------------------------------------------
+    shuffle and sort: aggregate values by keys
+    -------------------------------------------
+    a[1][2][6]   b[2][2]    c[5][9]
+      |             |           |
+       -------------------------
+                    |
+                 reducer
+                    |
+            a[9] b[4] c[14]
+
+MR in python:
+    class Mapper:
+        def map(docid, doc):
+            for term in doc:
+                emit(term, 1)
+
+    class Reducer:
+        def reduce(term, counts):
+            sum = 0
+            for count in counts:
+                sum += count
+            emit(term, sum)
+
 */
 package mr
