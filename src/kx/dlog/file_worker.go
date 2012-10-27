3a5da1e1835b0c2fff8365caea86e80b30139992
funky gao <funky.gao@gmail.com>
2012-10-27 13:48:36 +0800

    co-occurence of terms

--- src/kx/dlog/file_worker.go
+++ src/kx/dlog/file_worker.go
@@ -1,10 +1,19 @@
+// Find co-occurence of 2 terms in a file
+// Just a simplel demo
 package dlog
 
 import (
     "kx/mr"
     "kx/stats"
+    "strings"
     T "kx/trace"
 )
+
+type tuple [2]string
+
+func (this tuple) String() string {
+    return strings.TrimSpace(this[0]) + "," + strings.TrimSpace(this[1])
+}
 
 // Constructor of NoopWorker
 func NewFileWorker(manager *Manager, name, filename string, seq uint16) IWorker {
@@ -24,7 +33,17 @@ func (this *FileWorker) IsLineValid(line string) bool {
 // Extract meta info related to amf from a valid line
 func (this *FileWorker) Map(line string, out chan<- mr.KeyValue) {
     kv := mr.NewKeyValue()
-    kv[line] = 1.0
+    line = strings.TrimSpace(strings.Replace(line, "  ", " ", 0))
+    terms := strings.Split(line, " ")
+    for i, term := range terms {
+        for j:=i+1; j<len(terms); j++ {
+            var coOccurence tuple
+            coOccurence[0] = strings.TrimSpace(term)
+            coOccurence[1] = strings.TrimSpace(terms[j])
+            kv[coOccurence] = 1
+        }
+    }
+
     out <- kv
 }
 
@@ -36,7 +55,10 @@ func (this *FileWorker) Reduce(in mr.KeyValues) (out mr.KeyValue) {
 
     out = mr.NewKeyValue()
     for k, v := range in {
-        out[k.(string)] = stats.StatsSum(convertAnySliceToFloat(v))
+        var occurence = stats.StatsSum(convertAnySliceToInt(v))
+        if occurence > 1 {
+            out[k.(tuple)] = occurence
+        }
     }
 
     return
