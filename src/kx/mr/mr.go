89445f8cef14eb22fdc411b205978c3127c53d48
funkygao <funky.gao@gmail.com>
2012-10-29 18:40:44 +0800

    universal string or string slice Keys() func

--- src/kx/mr/mr.go
+++ src/kx/mr/mr.go
@@ -26,9 +26,16 @@ func (this KeyValue) Println() {
 	}
 }
 
-func (this KeyValue) DumpToSql() {
+// Self printable
+func (this KeyValue) PrintByOrderedKey(sortedKeys interface{}) {
+	for _, k := range sortedKeys.([]string) {
+		fmt.Printf(PRINT_FMT, T.CallerFuncName(1), k, this[k])
+	}
+}
+
+func (this KeyValue) DumpToSql(sortedKeys interface{}) {
 	println()
-	this.Println()
+	this.PrintByOrderedKey(sortedKeys)
 }
 
 // Self printable
@@ -38,14 +45,69 @@ func (this KeyValues) Println() {
 	}
 }
 
-func (this KeyValues) Keys() interface{} {
-	ks := make([]interface{}, len(this))
-	var i int
-	for k, _ := range this {
-		ks[i] = k
-	}
+func (this KeyValues) getOneKey() (key interface{}) {
+    for k, _ := range this {
+        key = k
+        return
+    }
 
-	return ks
+    return
+}
+
+func (this KeyValues) Keys() interface{} {
+    key := this.getOneKey()
+    if key == nil {
+        return nil
+    }
+
+    if _, ok := key.(string); ok {
+        keys := make([]string, len(this))
+        var i int
+        for k, _ := range this {
+            keys[i] = k.(string)
+            i ++
+        }
+
+        return keys
+    } else if _, ok := key.([2]string); ok {
+        keys := make([][2]string, len(this))
+        var i int
+        for k, _ := range this {
+            keys[i] = k.([2]string)
+            i ++
+        }
+
+        return keys
+    } else if _, ok := key.([3]string); ok {
+        keys := make([][3]string, len(this))
+        var i int
+        for k, _ := range this {
+            keys[i] = k.([3]string)
+            i ++
+        }
+
+        return keys
+    } else if _, ok := key.([4]string); ok {
+        keys := make([][4]string, len(this))
+        var i int
+        for k, _ := range this {
+            keys[i] = k.([4]string)
+            i ++
+        }
+
+        return keys
+    } else if _, ok := key.([5]string); ok {
+        keys := make([][5]string, len(this))
+        var i int
+        for k, _ := range this {
+            keys[i] = k.([5]string)
+            i ++
+        }
+
+        return keys
+    }
+
+	return nil
 }
 
 func (this KeyValues) Append(key interface{}, val interface{}) {
