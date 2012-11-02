19cf86586358480ad620430b7b61f7e562166acd
funkygao <funky.gao@gmail.com>
2012-11-02 20:19:53 +0800

    benchmark reflect

--- test/benchmark/benchmark_reflect.go
+++ test/benchmark/benchmark_reflect.go
@@ -7,12 +7,28 @@ import (
 )
 
 func main() {
-    fmt.Println(testing.Benchmark(benchmarkDeepEqual))
+    fmt.Println("deepEqual", testing.Benchmark(benchmarkDeepEqual))
+    fmt.Println("valueOf", testing.Benchmark(benchmarkValueOf))
+    fmt.Println("typeOf", testing.Benchmark(benchmarkTypeOf))
 }
+
+var foo = map[string]interface{} {"ff": 3.4}
 
 func benchmarkDeepEqual(b *testing.B) {
     x, y := []string{"a", "b"}, []string{"c", "d", "e"}
     for i:=0; i<b.N; i++ {
         reflect.DeepEqual(x, y)
+    }
+}
+
+func benchmarkValueOf(b *testing.B) {
+    for i:=0; i<b.N; i++ {
+        reflect.ValueOf(foo)
+    }
+}
+
+func benchmarkTypeOf(b *testing.B) {
+    for i:=0; i<b.N; i++ {
+        reflect.TypeOf(foo)
     }
 }
