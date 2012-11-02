59134a0c5d2d16a9234a6e8c848de224fd19bb8f
funkygao <funky.gao@gmail.com>
2012-11-02 20:49:51 +0800

    add error to return values

--- src/kx/stream/stream.go
+++ src/kx/stream/stream.go
@@ -3,6 +3,7 @@ package stream
 
 import (
     "bufio"
+    "errors"
     "os"
     "os/exec"
 )
@@ -12,48 +13,52 @@ func NewStream(mode StreamMode, name string, arg ...string) *Stream {
     return &Stream{name: name, arg: arg, mode: mode}
 }
 
-func (this *Stream) openPlainFile() {
+func (this *Stream) openPlainFile() error {
     file, err := os.Open(this.name)
     if err != nil {
-        panic(this.name + " not exist")
+        return err
     }
 
     this.reader = bufio.NewReader(file)
+    return nil
 }
 
-func (this *Stream) openExecPipe() {
+func (this *Stream) openExecPipe() error {
     this.cmd = exec.Command(this.name, this.arg...)
 
     // stdout pipe
     out, err := this.cmd.StdoutPipe()
     if err != nil {
-        panic(err)
+        return err
     }
 
     // stdin pipe
     in, err := this.cmd.StdinPipe()
     if err != nil {
-        panic(err)
+        return err
     }
 
     // startup
     if err := this.cmd.Start(); err != nil {
-        panic(err)
+        return err
     }
 
     // prepare the reader/writer
     this.reader = bufio.NewReader(out)
     this.writer = bufio.NewWriter(in)
+    return nil
 }
 
 // open stream
-func (this *Stream) Open() {
+func (this *Stream) Open() error {
     switch this.mode {
     case EXEC_PIPE:
-        this.openExecPipe()
+        return this.openExecPipe()
     case PLAIN_FILE:
-        this.openPlainFile()
+        return this.openPlainFile()
     }
+
+    return errors.New("non supported mode")
 }
 
 // get reader to read from the pipe output
@@ -67,11 +72,13 @@ func (this Stream) Writer() *bufio.Writer {
 }
 
 // close the stream
-func (this *Stream) Close() {
+func (this *Stream) Close() error {
     switch this.mode {
     case EXEC_PIPE:
         if err := this.cmd.Wait(); err != nil {
-            panic(err)
+            return err
         }
     }
+
+    return errors.New("current mode stream can't be closed")
 }
