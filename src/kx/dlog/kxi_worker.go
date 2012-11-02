57e339bfffc2503376fd4a0247bdea4942855757
funkygao <funky.gao@gmail.com>
2012-11-02 18:39:49 +0800

    cleaner json conversion

--- src/kx/dlog/kxi_worker.go
+++ src/kx/dlog/kxi_worker.go
@@ -48,40 +48,35 @@ func (this *KxiWorker) IsLineValid(line string) bool {
 }
 
 func (this *KxiWorker) Map(line string, out chan<- mr.KeyValue) {
-    const (
-        KEY_URL = "u"
-        KEY_RID = "i"
-        KEY_SERVICE = "s"
-        KEY_TIME = "t"
-        KEY_SQL = "q"
-    )
+    type jsonFromMapper struct {
+        Url string `json:"u"`
+        Rid string `json:"i"`
+        Service string `json:"s"`
+        Time float64 `json:"t"`
+        Sql string `json:"q"`
+    }
+
     var streamResult interface{}
     if streamResult = this.Worker.ExtractLineInfo(line); streamResult == nil {
         // this line is invalid
         return
     }
 
-    streamKv := make(map[string]interface{})
-    if err := json.Unmarshal([]byte(streamResult.(string)), &streamKv); err != nil {
+    j := new(jsonFromMapper)
+    if err := json.Unmarshal([]byte(streamResult.(string)), j); err != nil {
         panic(err)
     }
 
-    var (
-        url, rid, service, sql string
-        time float64
-    )
-    url, rid, service, time, sql = streamKv[KEY_URL].(string), streamKv[KEY_RID].(string),
-        streamKv[KEY_SERVICE].(string), streamKv[KEY_TIME].(float64),
-        streamKv[KEY_SQL].(string)
     if this.manager.option.debug {
-        fmt.Fprintf(os.Stderr, "DEBUG<= %s %s %s %f %s\n", url, rid, service, time, sql)
+        fmt.Fprintf(os.Stderr, "DEBUG<= %s %s %s %f %s\n",
+            j.Url, j.Rid, j.Service, j.Time, j.Sql)
     }
 
     kv := mr.NewKeyValue()
-    kv[[KXI_M_KEYLEN]string{mr.KEY_GROUP, "url call kxi service", url, service}] = time
-    kv[[KXI_M_KEYLEN]string{mr.KEY_GROUP, "url within a request", url, rid}] = time
-    kv[[KXI_M_KEYLEN]string{mr.KEY_GROUP, "url query db sql", url, sql}] = time
-    kv[[KXI_M_KEYLEN]string{mr.KEY_GROUP, "kxi servants", service, ""}] = time
+    kv[[KXI_M_KEYLEN]string{mr.KEY_GROUP, "url call kxi service", j.Url, j.Service}] = j.Time
+    kv[[KXI_M_KEYLEN]string{mr.KEY_GROUP, "url within a request", j.Url, j.Rid}] = j.Time
+    kv[[KXI_M_KEYLEN]string{mr.KEY_GROUP, "url query db sql", j.Url, j.Sql}] = j.Time
+    kv[[KXI_M_KEYLEN]string{mr.KEY_GROUP, "kxi servants", j.Service, ""}] = j.Time
     out <- kv
 }
 
