8eb0104d916e3ddf94c8395357115e5889aa0530
funky gao <funky.gao@gmail.com>
2012-11-04 17:01:34 +0800

    benchmark includes stdlib

--- test/benchmark/bench.sh
+++ test/benchmark/bench.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+echo ========================
+echo stdlib benchmark
+echo ========================
+go test -test.bench=".*" fmt
+go test -test.bench=".*" bytes
+go test -test.bench=".*" strconv
+go test -test.bench=".*" regexp
+
+echo
+echo
+
 for f in `ls benchm*.go`
 do
     echo $f
