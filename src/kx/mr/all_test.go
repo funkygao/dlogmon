2ee394ec1c58dc539f4c7726eea028688bb8f485
funky gao <funky.gao@gmail.com>
2012-11-04 18:05:43 +0800

    append test case

--- src/kx/mr/all_test.go
+++ src/kx/mr/all_test.go
@@ -1,6 +1,7 @@
 package mr
 
 import (
+    "fmt"
     "github.com/bmizerany/assert"
     "testing"
 )
@@ -13,6 +14,18 @@ func TestAppend(t *testing.T) {
 
     f := ConvertAnySliceToFloat(d["a"])
     assert.Equal(t, f, []float64{1.0, 2.0})
+}
+
+func TestAppendVariadics(t *testing.T) {
+    d := NewKeyValues()
+    d.Append("a", 1.0, 2., 3.)
+    d.Append("b", 3.0)
+    fmt.Println(d)
+
+    f := ConvertAnySliceToFloat(d["a"])
+    assert.Equal(t, f, []float64{1., 2.0, 3})
+    f = ConvertAnySliceToFloat(d["b"])
+    assert.Equal(t, f, []float64{3})
 }
 
 func TestKeyValueSortType(t *testing.T) {
