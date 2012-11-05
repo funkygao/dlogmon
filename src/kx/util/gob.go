00463d944f3b89b315011c3b81aa310098ff14e9
funkygao <funky.gao@gmail.com>
2012-11-05 18:04:45 +0800

    util to serialize/de slice to hashable string

---
+++ src/kx/util/gob.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+    "bytes"
+    "encoding/gob"
+)
+
+var (
+    buf bytes.Buffer
+    enc = gob.NewEncoder(&buf)
+    dec = gob.NewDecoder(&buf)
+)
+
+func EncodeStrSlice(s []string) (r string, e error) {
+    buf.Reset()
+    if e = enc.Encode(s); e != nil {
+        return
+    }
+    r = buf.String()
+    return
+}
+
+func DecodeStrToSlice(s string) (r []string, e error) {
+    buf.Reset()
+    buf.WriteString(s)
+    r = make([]string, 0)
+    if e = dec.Decode(&r); e != nil {
+        return
+    }
+    return
+}
