bdb8d0a2855e4317aa4f9fb5633ec107cdf613f3
funky gao <funky.gao@gmail.com>
2012-11-06 22:53:18 +0800

    const of how to use dlogmon cli

--- src/kx/dlogmon/const.go
+++ src/kx/dlogmon/const.go
@@ -3,4 +3,8 @@ package main
 const (
     maxprocsenv = "GOMAXPROCS"
     VERSION = "1.0.8r"
+    DLOGMON_INTRO = `dlogmon - kaixin dlog parser and report generator
+Enter 'help' to see how to use it
+    `
+    DLOOGMON_PROMPT = "(dlog) "
 )
