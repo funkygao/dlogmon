c3d92a4c5bc8411a6a90da73a11d6b9245abcfaa
funkygao <funky.gao@gmail.com>
2012-11-07 10:39:49 +0800

    prompt like a shell

--- src/kx/dlogmon/const.go
+++ src/kx/dlogmon/const.go
@@ -9,6 +9,6 @@ const (
 dlogmon - kaixin dlog parser and report generator
 Enter "help | h" to see how to use it
 =================================================`
-    DLOGMON_PROMPT = "(dlog) "
+    DLOGMON_PROMPT = "(dlog)$ "
     DLOGMON_HELP = `Use "help | h [command]" for more information about a command.`
 )
