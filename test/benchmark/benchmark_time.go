245636e06c041041d85cb819ab81e9b5deecc16b
funkygao <funky.gao@gmail.com>
2012-11-08 18:33:55 +0800

    benchmark sleep

--- test/benchmark/benchmark_time.go
+++ test/benchmark/benchmark_time.go
@@ -8,10 +8,17 @@ import (
 
 func main() {
     fmt.Printf("%s\n", testing.Benchmark(benchmarkNow).String())
+    fmt.Printf("%s\n", testing.Benchmark(benchmarkSleep).String())
 }
 
 func benchmarkNow(b *testing.B) {
     for i:=0; i<b.N; i++ {
         time.Now()
+    }
+}
+
+func benchmarkSleep(b *testing.B) {
+    for i:=0; i<b.N; i++ {
+        time.Sleep(0)
     }
 }
